7929fa2f5d842b70c3e9b0ab9b06497cdb06d0ec
KShida <57141108+KShida@users.noreply.github.com>
2019-11-14 13:54:44 +0300

    изменения в дз 5

--- Lesson5/Lesson5.java
+++ Lesson5/Lesson5.java
@@ -1,43 +1,26 @@
 package ru.geekbrains.lesson5;
 
 import ru.geekbrains.lesson5.animals.*;
-import ru.geekbrains.lesson5.animals.dogs.Husky;
-import ru.geekbrains.lesson5.animals.dogs.Spitz;
 
 public class Lesson5 {
 
     public static void main(String[] args) {
         Animal dog = new Dog("Guzzler");
-        Animal dog1 = new Husky("Hunter");
-        Animal dog2 = new Spitz("Tot");
+        Animal dog1 = new Dog("Hunter");
+        Animal dog2 = new Dog("Tot");
         Animal bird = new Bird("Thief");
         Animal horse = new Horse("Rapid");
         Animal cat = new Cat("Impudent");
-        Animal dolphin = new Dolphin("Tsunami");
 
-        dog1.run(800);
+        dog1.run(600);
         System.out.println();
 
-        Animal[] animals = { dog, dog1, dog2, bird, horse, cat, dolphin};
+        Animal[] animals = { dog, dog1, dog2, bird, horse, cat};
 
         for (Animal animal : animals) {
-
             animal.run(550);
             animal.jump(0.5);
             animal.swim(10);
-
-            if (animal instanceof Dog) {
-                ((Dog) animal).speak();
-            }
-
-            if (animal instanceof Cat) {
-                ((Cat) animal).speak();
-            }
-
-            if (animal instanceof Bird) {
-                ((Bird) animal).action();
-            }
-
             System.out.println();
         }
     }
--- Lesson5/animals/Dog.java
+++ Lesson5/animals/Dog.java
@@ -1,18 +1,23 @@
 package ru.geekbrains.lesson5.animals;
 
 import ru.geekbrains.lesson5.Animal;
+import java.util.Random;
 
 public class Dog extends Animal {
 
-    public Dog(String name) {
-            super(name);
-            lengthRunMax = 500;
-            heightJumpMax = 0.5;
-            lengthSwimMax = 10;
+    private double run;
+
+    public Dog () {
+        Random random = new Random();
+        run = random.nextDouble() * (800.01 - 1.0) + 400.0;
     }
 
-    public void speak() {
-        System.out.println(name + " say Woof, woof, woof! :P");
+    public Dog(String name) {
+        super(name);
+        this ();
+        lengthRunMax = run;
+        heightJumpMax = 0.5;
+        lengthSwimMax = 10;
     }
 
 }
